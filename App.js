28835d410e3dd3eb253d3bca29d68f982506c3e8
Najeeb <naxibkhan@gmail.com>
2020-07-06 01:32:28 +0500

    component added

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import ChatScreen from './src/screens/ChatScreen';
 import LoginScreen from './src/screens/LoginScreen';
-import ImageScreen from './src/screens/ImageScreen';
 import CounterScreen from './src/screens/CounterScreen';
-import ColorScreen from './src/screens/ColorScreen';
-import SquareScreen from './src/screens/SquareScreen';
-import TextScreen from './src/screens/TextScreen';
-import BoxScreen from './src/screens/BoxScreen';
+
 
 const Stack = createStackNavigator();
 
